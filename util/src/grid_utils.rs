5f1ebb6c02825b03668d7966fba74d5629993f77
RG <goyalrohan2020@gmail.com>
2024-12-07 17:48:28 +0530

    feat(util): grid finder and line getter

--- util/src/grid_utils.rs
+++ util/src/grid_utils.rs
@@ -1,10 +1,18 @@
-pub fn find_in_grid<T: PartialEq>(grid: &Vec<Vec<T>>, value: &T) -> Option<(usize, usize)> {
+pub fn find_in_grid<T: PartialEq>(grid: &Vec<Vec<T>>, search_value: &T) -> Option<(usize, usize)> {
     for (y, row) in grid.iter().enumerate() {
         for (x, value) in row.iter().enumerate() {
-            if *value == *value {
-                return Some((x, y));
+            if *value == *search_value {
+                return Some((y, x));
             }
         }
     }
     None
+}
+
+pub fn get_horizontal_line<T: std::fmt::Debug>(grid: &Vec<Vec<T>>, y: usize) -> Vec<&T> {
+    grid.get(y).unwrap().iter().collect()
+}
+
+pub fn get_vertical_line<T>(grid: &Vec<Vec<T>>, x: usize) -> Vec<&T> {
+    grid.into_iter().map(|row| &row[x]).collect()
 }
