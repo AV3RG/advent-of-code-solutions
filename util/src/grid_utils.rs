998726a063d1f00d3a0e683a0765c94d55fc163f
RG <goyalrohan2020@gmail.com>
2024-12-11 19:42:18 +0530

    feat(utils): grid printing and neighbours

--- util/src/grid_utils.rs
+++ util/src/grid_utils.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::fmt::Debug;
 use std::hash::Hash;
 
 pub fn find_first_in_grid<T: PartialEq>(grid: &Vec<Vec<T>>, search_value: &T) -> Option<(usize, usize)> {
@@ -46,8 +47,31 @@ pub fn grid_item_positions<T: Eq + Hash>(grid: &Vec<Vec<T>>) -> HashMap<&T, Vec<
     result
 }
 
+pub fn print_grid<T: Debug>(grid: &Vec<Vec<T>>) {
+    for row in grid.iter() {
+        println!("{:?}", row);
+    }
+}
 
-pub fn get_horizontal_line<T: std::fmt::Debug>(grid: &Vec<Vec<T>>, y: usize) -> Vec<&T> {
+pub fn get_manhattan_neighbours(pos: (usize, usize), m: usize, n: usize) -> Vec<(usize, usize)> {
+    let mut result = Vec::new();
+    let (y, x) = pos;
+    if x > 0 {
+        result.push((y, x - 1));
+    }
+    if x < m - 1 {
+        result.push((y, x + 1));
+    }
+    if y > 0 {
+        result.push((y - 1, x));
+    }
+    if y < n - 1 {
+        result.push((y + 1, x));
+    }
+    result
+}
+
+pub fn get_horizontal_line<T>(grid: &Vec<Vec<T>>, y: usize) -> Vec<&T> {
     grid.get(y).unwrap().iter().collect()
 }
 
